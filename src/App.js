1050566e48b2df3f0ba4c03029a23be67bf9586d
tylerjusfly <tylerjusfly1@gmail.com>
2022-08-20 19:02:07 +0100

    prettierrc fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Notfound from './components/NotFound'
-import Tickets from './components/Tickets'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Notfound from './components/NotFound';
+import Tickets from './components/Tickets';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='ticket/:ticketId' element ={ <Tickets/>}/>
-      <Route path='*' element ={ <Notfound/> }/>
-    </Routes>
+      <Routes>
+        <Route path="ticket/:ticketId" element={<Tickets />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
